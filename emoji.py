33efa3521db1f0ab5f9b972a2fd3be584bbdacd7
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2019-11-16 21:00:35 -0700

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,13 @@
+# x = 1
+# num_row = 20
+# while x <= num_row:
+# 	print(''*(num_row-x)+("*"*x))
+# 	x+=2
+
+for num in range(1,11):
+	print("*" * num)
+
+times = 1
+while times < 11:
+	print("*" * times)
+	times += 1
